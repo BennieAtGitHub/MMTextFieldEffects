8f33388006da6be728111cb64aee42fb86946507
Bennett <hanspy.cn@gmail.com>
2015-08-25 11:14:48 +0800

    add header file

---
+++ MMTextFieldEffects/Classes/MMTextFieldEffects.h
@@ -0,0 +1,16 @@
+//
+//  MMTextFieldEffects.h
+//  MMTextFieldEffects
+//
+//  Created by Bennett on 15/8/25.
+//  Copyright (c) 2015年 madapps. All rights reserved.
+//
+
+#import "BetaNaoTextField.h"
+#import "IchiroTextField.h"
+#import "JuroTextField.h"
+#import "KuroTextField.h"
+#import "MinoruTextField.h"
+#import "NarikoTextField.h"
+#import "YoshikoTextField"
+
